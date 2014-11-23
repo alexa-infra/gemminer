8a4636a16cd982c8c64f2c476f7015373bab425e
Alexey Vasilyev <alexa.infra@gmail.com>
2014-11-23 02:21:17 +0100

    singleton class

---
+++ src/base/singleton.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "base/types.h"
+#include "base/debug.h"
+
+namespace base
+{
+
+template<class Type>
+class Singleton
+{
+public:
+    template<typename... Args>
+    static void init(Args&... args) {
+        ASSERT( hasInstance() == false );
+        instance_ = new Type(args...);
+    }
+    static void shutdown() {
+        ASSERT( hasInstance() == true );
+        delete instance_;
+        instance_ = nullptr;
+    }
+    static bool hasInstance() {
+        return instance_ != nullptr;
+    }
+    static Type& instance() {
+        ASSERT(hasInstance() == true);
+        return *instance_;
+    }
+protected:
+    Singleton() {}
+
+    static Type* instance_;
+};
+
+} // namespace base
